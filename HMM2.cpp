5edf41a6dfd8168513e9eca648fd2374d4ee2ea0
NiklasRolleberg <niklasr93@gmail.com>
2014-10-04 18:53:47 +0200

    klar

--- HMM2.cpp
+++ HMM2.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 #include <cstdlib>
-#include <sstream> 
+#include <sstream>
 
-struct matrix 
+struct matrix
 {
 	int row;
 	int col;
 	double** m;
-	
+
 	//constructor
 	matrix(double** mat, int rows, int cols)
 	{
@@ -17,40 +18,48 @@ struct matrix
 		col = cols;
 		m = mat;
 	}
-	
+
 	//constructor 2
 	matrix(std::string in)
 	{
 		std::istringstream iss;
 		iss.str(in);
 		iss >> row >> col;
-		
-		m = (double**)calloc(row , sizeof(double *));
-		for(int i=0 ; i< row ; ++i)
-			m[i] = (double*)calloc(col , sizeof(double));
-		
+
+        m = initialize(row,col);
+
 		for(int i=0;i<row;++i)
 		{
 			for(int j=0;j<col;++j)
 			iss >> m[i][j];
 		}
 	}
-	
+
+    //creted the 2d matrix
+	double** initialize(int rows, int cols)
+	{
+	    double** temp;
+	    temp = (double**)calloc(rows , sizeof(double *));
+		for(int i=0 ; i< rows ; ++i)
+			temp[i] = (double*)calloc(cols , sizeof(double));
+        return temp;
+	}
+
 	double get(int r, int c)
 	{
 		return m[r][c];
 	}
-	
-	void print() 
+
+	void print()
 	{
 		for (int i=0;i<row;++i)
 		{
 			for(int j=0;j<col;++j)
 				std::cout << m[i][j] <<" ";
 			std::cout << std::endl;
-		}		
+		}
 	}
-	
+
 	void writeMatrix()
 	{
 		std::cout << row << " " << col;
@@ -59,68 +68,42 @@ struct matrix
 				std::cout << " " << m[i][j];
 		}
 	}
-	
-	void transpose()
+
+	matrix transpose()
 	{
 		int i,j;
-		double* temp;
-		for (i = 0; i < row/2; ++i) 
-		{
-			temp = m[row-i-1];
-			m[row-i-1] = m[i];
-			m[i] = temp;
-			
-			double temp2;
-			
-			for (j = 0; j < col/2; ++j) 
-			{
-				temp2 = m[i][col-i-1];
-				m[i][row-i-1] = m[i][j];
-				m[i][j] = temp2;
-			}
-			
-			for (j = 0; j < col/2; ++j) 
-			{
-				temp2 = temp[col-i-1];
-				temp[row-i-1] = temp[j];
-				temp[j] = temp2;
-			}
-
-			m[i] = temp;
-		}
-		int a = col;
-		row = col;
-		col = a;
+		double** temp = initialize(col,row);
+		for(i=0;i<row;++i)
+        {
+            for(j=0;j<col;++j)
+                temp[j][i] = m[i][j];
+        }
+        return matrix(temp,col,row);
 	}
-	
+
 	matrix getCol(int index)
 	{
 		int newRows = row;
 		int newCols = 1;
 
-		double** temp;
-		temp = (double**)calloc(newRows , sizeof(double *));
-		for(int i=0 ; i< newRows ; ++i)
-			temp[i] = (double*)calloc(newCols , sizeof(double));
-			
+		double** temp = initialize(newRows,newCols);
+
 		for(int i=0;i<newRows;++i)
 			temp[i][0] = m[i][index];
 		return matrix(temp,newRows,newCols);
 	}
-	
-	matrix operator*(matrix H)
+
+	matrix operator*(matrix &H)
 	{
 		int newRows = row;
 		int newCols = H.col;
-		
+
 		if(col != H.row)
 			std::cerr << "Fel. matrix 1: " << row << " " << col << ", matrix 2: " << H.row << " " << H.col << std::endl;
 
 		double** temp;
-		temp = (double**)calloc(newRows , sizeof(double *));
-		for(int i=0 ; i< newRows ; ++i)
-			temp[i] = (double*)calloc(newCols , sizeof(double));
-		
+		temp = initialize(newRows,newCols);
+
 		//RÄKNA!
 		double t;
 		int i,j,k;
@@ -142,15 +125,12 @@ struct matrix
 	{
 		int newRows = row;
 		int newCols = col;
-		
-		if(row!=H.row || col != H.col)
-			std::cout << "FEL" << row << " " << H.row << " , " << col << H.col <<std::endl;
 
-		double** temp;
-		temp = (double**)calloc(newRows , sizeof(double *));
-		for(int i=0 ; i< newRows ; ++i)
-			temp[i] = (double*)calloc(newCols , sizeof(double));
-		
+		if(row!=H.row || col != H.col)
+			std::cout << "FEL " << row << " " << H.row << " , " << col <<" " <<H.col <<std::endl;
+
+		double** temp = initialize(newRows,newCols);
+
 		//RÄKNA!
 		double t;
 		int i,j,k;
@@ -161,27 +141,34 @@ struct matrix
 		}
 		return matrix(temp,newRows,newCols);
 	}
-	
+
 };
 
 int main(int argc, char **argv)
 {
-	// Read the file	
+	// Read the file
 	std::vector<std::string> board;
 	for (std::string line; std::getline(std::cin, line);)
 		board.push_back(line);
-		
+
 	matrix A = matrix(board[0]);
 	matrix B = matrix(board[1]);
 	matrix q = matrix(board[2]);
-		
-	//
-	std::cout <<" innan " << std::endl;
-	A.print();
-	std::cout <<" efter " << std::endl;
-	A.transpose();
-	//
-	
+
+	//Felkoll
+
+    /*
+    std::cout << "\nA" <<std::endl;
+    A.print();
+    std::cout << "\nB" <<std::endl;
+    B.print();
+    std::cout << "\nq" <<std::endl;
+    q.print();
+
+	//matrix X = A*B*A*B*A*B*A*B*(q*A).transpose(); //ger rätt svar
+    //X.print();
+    */
+
 	//find out the statesequence
 	int Nstates;
 	int index;
@@ -189,21 +176,23 @@ int main(int argc, char **argv)
 	iss.str(board[3]);
 	iss >> Nstates;
 	iss >> index;
-	
-	matrix L = q/B.getCol(index); 
-	L.print();
+
+	matrix L = q/B.getCol(index).transpose();
+	//L.print();
 	for(int i=1;i<Nstates;++i)
 	{
 		iss >> index;
-		std::cout << index << std::endl;
-		matrix T = B.getCol(index);
-		L = (L*A)/T;
-		L.print();
+		//matrix T = B.getCol(index).transpose();
+		L = (L*A)/(B.getCol(index).transpose());
+		//L.print();
 	}
-	
-	//matrix P = q*A;
-	//matrix Po = P*B;
-	//Po.writeMatrix();
+
+	//sum the elements in L
+	double m = 0;
+	for(int i=0;i<L.col;++i)
+            m += L.get(0,i);
+    double t = round(m*pow(10,6))/pow(10,6);
+    std::cout << t << std::endl;
 	return 0;
 }
 
